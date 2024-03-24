e292ef92491183cdff2009e9d552bdeb33311db1
Justice Engle <justice.engle@icloud.com>
2024-03-23 20:48:09 -0400

    Add project files.

---
+++ WebScraper/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WebScraper.Data;
+
+namespace WebScraper;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddDbContext<WebScraperContext>();
+
+        IHost app = builder.Build();
+
+        var _context = app.Services.GetRequiredService<WebScraperContext>();
+
+        _context.Database.EnsureDeleted();
+        _context.Database.EnsureCreated();
+
+        app.RunAsync();
+    }
+}
